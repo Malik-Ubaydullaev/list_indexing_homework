07689a050cb1bb235ced60c5ba3ca620d6850238
Your Name <ubaydullayev@samtuit.uz>
2022-01-09 21:09:08 +0500

    9-misol

--- List09.py
+++ List09.py
@@ -1,3 +1,10 @@
+def is_same(list1):
+    if list1[0] == list1[1] and list1[0] == list1[2] and list1[0] == list1[3] and list1[0] == list1[4] and list1[0] == list1[5]:
+        return True
+    else:
+        return False
+    
+    
 def main(list1):
     """
     A list of several elements is given. True if all items are the same, otherwise return False.
@@ -6,4 +13,4 @@ def main(list1):
     Returns:
         bool: return answer
     """
-    return
+    return is_same(list1)
