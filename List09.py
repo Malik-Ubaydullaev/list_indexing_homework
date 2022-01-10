ae4f35aece976a62633dac6ab33065dc694ad9a6
Your Name <ubaydullayev@samtuit.uz>
2022-01-10 15:39:54 +0500

    9-misol

--- List09.py
+++ List09.py
@@ -1,7 +1,16 @@
 def is_same(list1):
     if len(list1) == 1:
         return True
-    elif list1[0] == list1[1] and list1[0] == list1[2] and list1[0] == list1[3] and list1[0] == list1[4]:
+    elif len(list1) == 5 and list1[0] == list1[1] and list1[0] == list1[2] and list1[0] == list1[3] and list1[0] == list1[4]:
+        return True
+    
+    elif len(list1) == 4 and list1[0] == list1[1] and list1[0] == list1[2] and list1[0] == list1[3]:
+        return True
+    
+    elif len(list1) == 3 and list1[0] == list1[1] and list1[0] == list1[2]:
+        return True
+    
+    elif len(list1) == 2 and list1[0] == list1[1]:
         return True
     else:
         return False
